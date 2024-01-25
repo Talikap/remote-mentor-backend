67ea14b377d0edd98bd418a2c0c074c4a0f85cce
talikap <talikaplan12@gmail.com>
2024-01-25 17:45:45 +0200

    deploy

--- server.js
+++ server.js
@@ -11,29 +11,27 @@ const socketHandler = require('./socketHandler')
 
 //express app
 const app = express()
-
-
-app.use(cors({
-  origin:'https://remote-mentor-production-fd03.up.railway.app/', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200,
-}))
 app.use(express.json())
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
+  const corsOptions = {
+      origin: ['http://127.0.0.1:3000', 'http://localhost:3000'], 
+      credentials: true
+  }
+  app.use(cors(corsOptions))
+}
+
 
 //app.use(express.urlencoded({ extended: true }))
 app.use('/api/codeblocks', codeBlockRoutes)
-app.get('/**', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'), function(err) {
-    if (err) {
-      res.status(500).send(err);
-    }
-  });
-});
 
-const port = process.env.PORT || 4000
-app.listen(port,"0.0.0.0", () =>{
-  console.log(`listening on port ${port}`) 
+app.get('/**', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
+
+
 //const corsOptions = {
   //  origin: 'https://remote-mentor-production.up.railway.app', // Replace with your React app's URL
    // optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
@@ -78,3 +76,8 @@ mongoose.connect(process.env.MONGO_URI)
     .catch((error) => {
         console.log(error)
     })
+
+  const port = process.env.PORT || 4000
+  app.listen(port,"0.0.0.0", () =>{
+    console.log(`listening on port ${port}`) 
+})
